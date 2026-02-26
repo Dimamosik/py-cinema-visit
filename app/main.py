3b053a8e1a0e47c5aec7c7d46a9e9b6c4045bb6c
Acer <konskiidima@gmail.com>
2026-02-26 13:34:56 +0100

    py-cinema-visit commit3

--- app/main.py
+++ app/main.py
@@ -1,22 +1,30 @@
-# write your imports here
+from typing import Dict, List
+
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
-from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
-def cinema_visit(customers, hall_number, cleaner, movie):
+
+def cinema_visit(
+    customers: List[Dict[str, str]],
+    hall_number: int,
+    cleaner: str,
+    movie: str,
+) -> None:
     customer_instances = [
-        Customer(name=customer["name"], food=customer["food"],)
+        Customer(name=customer["name"], food=customer["food"])
         for customer in customers
     ]
+
     for customer in customer_instances:
         CinemaBar.sell_product(product=customer.food, customer=customer)
 
     hall = CinemaHall(number=hall_number)
-    cleaning_staff= Cleaner(name=cleaner)
+    cleaning_staff = Cleaner(name=cleaner)
 
     hall.movie_session(
         movie_name=movie,
         customers=customer_instances,
-        cleaning_staff=cleaning_staff
+        cleaning_staff=cleaning_staff,
     )
